2e7e10b70c4bb9d00d507ef322b6c2fccea61443
jinmanc <jinmanc@yahoo.com>
2020-05-04 13:52:51 -0500

    location

--- starsnow/meals/urls.py
+++ starsnow/meals/urls.py
@@ -6,5 +6,6 @@ app_name = 'meals'
 urlpatterns = [
     path('',views.home, name = 'starsnowice-home'),
     path('meals/', views.meal_list, name = 'meal_list'),
-    path('meals/<slug:slug>',views.meal_detail, name='meal_detail')
+    path('meals/<slug:slug>',views.meal_detail, name='meal_detail'),
+    path('location/', views.location, name = 'location'),
     ]
